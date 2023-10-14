33e02303abc46fb3c21b9cd574d93fb693e33626
Tanya Twinkle <90620194+tanyatwinkle@users.noreply.github.com>
2023-10-14 23:07:11 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
+import React from 'react';
+import './App.css'; // Import the CSS file for styling
+
+function WordCounter() {
+  const [state, setState] = React.useState({ 
+    charCount: 0
+  });
+  const handleKeyPress = (e) => {
+    const count = e.target.value;
+    setState({
+      charCount: count.length
+    });
+  };
+  return (
+    <div>
+  <p class="ul"><label for="w3review"><h1>Responsive Paragraph Word Counter</h1></label></p>
+  <textarea placeholder='Type here' onChange={handleKeyPress} rows="5" cols="20" >    
+    </textarea>
+    <br/>
+  <span>Word Count: {state.charCount}</span>
+  <br/>
+   </div>
+  );
+  };
+function App() {
+  return (
+    <WordCounter />
+  );
+}
+
+export default App;
 
